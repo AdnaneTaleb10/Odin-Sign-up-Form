8609b4dfb1a7135c862394a0c814bef57cb19a6c
Adnane Taleb <adnane102464@gmail.com>
2024-08-25 18:22:11 +0100

    Add input border reset and dynamic validation styles in JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-let inputElements = document.querySelectorAll('input');
-inputElements = Array.from(inputElements);
+let password = document.querySelector('#password');
+let confirmPassword = document.querySelector('#confirm-password');
+let createAccountButton = document.querySelector('button');
+let inputs = document.querySelectorAll('input');
 
+inputs.forEach(input => {
+    input.style.border = '2px solid black';
 
-inputElements.forEach((input) => {
-    let focused = false;
-    input.addEventListener('click' , (e) => {
-        focused = true;
-        
-        if(focused && e.target.value == ''){
-            e.target.style.borderColor = 'red';
+    input.addEventListener('input', () => {
+        if (input.checkValidity()) {
+            input.style.border = '2px solid green'; 
+        } else {
+            input.style.border = '2px solid red'; 
         }
-    })
-})
+    });
+});
 
-
+createAccountButton.addEventListener('click', (e) => {
+    if(password.value !== '' && password.value !== confirmPassword.value){
+        confirmPassword.style.border = '2px solid red';
+        e.preventDefault();
+    }
+});
